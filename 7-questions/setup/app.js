d3390e41aba352209f1cd0e1721a290fbc26dce9
Marija Cehic <cehicmarija@gmail.com>
2020-10-18 01:05:56 +0200

    finished #7, started #8

--- 7-questions/setup/app.js
+++ 7-questions/setup/app.js
@@ -1,2 +1,33 @@
+// traversing the dom -- moving up and down the DOM tree
+// const btns= document.querySelectorAll('.question-btn'); //bc there are multiple buttons
+
+// btns.forEach(function (btn){
+//     btn.addEventListener('click', function(e){
+//        const question = e.currentTarget.parentElement.parentElement;// climbing up the DOM TREE
+//        question.classList.toggle("show-text"); 
+    
+//     });
+// });
+
+// ctrl + k + c -- comment multiple lines
+//ctr + k + u -- uncomment them
+
+
 //using selectors inside the element
-// traversing the dom
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function(question){ // this question accesses the entire article (title, text and button)
+const btn = question.querySelector(".question-btn");// to look for the exact button that was clicked, and not all of them
+btn.addEventListener('click', function(){
+    // functioanlity to close otheyr articles when one is opened:
+    questions.forEach(function(item){ //forEach is a loop
+        if(item !== question){
+            item.classList.remove('show-text');
+        }
+    });
+
+
+    question.classList.toggle("show-text");
+})
+});
